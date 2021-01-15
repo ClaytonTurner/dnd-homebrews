4c35f7376688e00f03ac0f8c675d1e9ff93da7a0
rfboykin <rfboykin@gmail.com>
2021-01-14 21:03:48 -0600

    Update spells.js
    Add Detect Life and Eldath's Love variant of Sacred Flame

--- spells.js
+++ spells.js
@@ -36,3 +36,73 @@ SpellsList["claw's pillars of lightning"] = {
 	psionic : false,
 
 };
+
+SpellsList["Detect Life"] = {
+
+	name: "Detect Life",
+	nameAlt: "Detect Life",
+	nameShort: "Detect Life",
+
+	classes: ["druid", "paladin", "ranger"], // Cleric ommitted for flavor below
+
+	source: ["HB", 0],
+
+	level: 2,
+
+	school: "Divination",
+
+	time: "1 a",
+
+	range: "Self",
+
+	components: "V,S",
+
+	duration: "Conc, up to 10 min",
+
+	description: "Sense living creatures in 30ft radius; Can discern aura and whether heathly; penetrates barriers but blocked by thicc ones;",
+
+	descriptionFull: "For the duration, you sense the presence of living creatures within 30 feet of you. This spell has no effect on undead or constructs. If a creature in the area is not visible to you, you know its distance and direction.\nIf you sense a creature in this way, you can use your action to discern an aura surrounding the creature that tells you its type and whether it is healthy. A creature will register as healthy if it has more than half of its hit points left and is not suffering from the conditions of disease, poison, or exhaustion.\nThe spell can penetrate most barriers, but it is blocked by 1 foot of stone, 1 inch of common metal, a thin sheet of lead, or 3 feet of wood or dirt.",
+
+	ritual: true,
+
+	psionic: false,
+
+};
+
+SpellList["Detect Life - cleric"] = SpellsList["Detect Life"];
+SpellsList["Detect Life - cleric"].name = "Find the gifts of Her Holiness"
+SpellsList["Detect Life - cleric"].classes = ["cleric"]
+
+SpellsList["Eldaths' Love"] = {
+
+	name: "Eldath's Love (Sacred Flame)",
+
+	nameAlt: "Sacred Flame",
+
+	classes: ["cleric"],
+
+	source: ["P", 272],
+
+	level: 0,
+
+	school: "Evocatoin",
+
+	save: "Dex",
+
+	time: "1 a",
+
+	range: "60ft",
+
+	components: "V,S",
+
+	duration: "Instantaneous",
+
+	description: "1 creature save or 2d8 Radiant dmg; no bonus for cover on save; +1d8 at CL 11, 17",
+
+	descriptionFull: "Flame-like radiance descends on a creature that you can see within range. The target must succeed on a Dexterity saving throw or take 1d8 radiant damage. The target gains no benefit from cover for this saving throw.",
+
+	ritual: false,
+
+	psionic: false,
+
+};
