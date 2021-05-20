e37a64e648e930e08a755fe3f731f73e0d09e312
Clayton Turner <clayton@redshelf.com>
2021-05-20 17:05:58 -0500

    Add Claw's Crystal Aero Evocation Spell

--- CoS/spells.js
+++ CoS/spells.js
@@ -2,7 +2,7 @@
 var iFileName = "Homebrew - SpellsList.js"; // Optional; This is how the file will be named in the sheet if you import it as a file and not copy-paste its content. Only the first occurrence of this variable will be used
 RequiredSheetVersion(12.999); // Optional; This is the minimum required version number of the sheet for the script to work. If the sheet being used to import the script is of an earlier version, the user will be warned
 
-SpellsList["claw's pillars of lightning"] = { 
+SpellsList["claw's pillars of lightning"] = {
 
 	name : "Claw's Pillars of Lightning",
 	nameAlt : "Lightning Pillars",
@@ -30,6 +30,41 @@ SpellsList["claw's pillars of lightning"] = {
 	description : "3 fur tufts; any creature contacting engulfed in 5ft wide 10ft high electricity; 6d6 lightning damage; save halves",
 
 	descriptionFull : "Currents of wind scatter tufts of charged fur to three different unoccupied locations within range. When a creature enters a space occupied by one of these tufts for the first time on a turn or starts its turn there, the creature is engulfed by a 5-foot-radius, 10-foot-high pillar of discharged electricity and must make a Constitution saving throw as the current courses through its body. On a failed save, the creature takes 6d6 lightning damage and its speed is reduced to 0 for the rest of the turn. On a successful save, the creature takes half damage and its speed is not reduced." + "\n   " + "The tufts of fur are nearly invisible and require a successful Intelligence (Investigation) check against your spell save DC to determine their location." + "\n   " + "On each of your turns after you cast this spell, you may use a bonus action to move one of the tufts of fur to another unoccupied location within range." + "\n   " +"At Higher Levels: When you cast this spell using a spell slot of 5th level or higher, the number of charged tufts released increases by one for each spell level above 4th.",
+
+	ritual : false,
+
+	psionic : false,
+
+};
+
+SpellsList["claw's crystal aero"] = {
+
+	name : "Claw's Crystal Aero",
+	nameAlt : "Crystal Aero",
+
+	classes : ["sorcerer", "wizard"],
+
+	source : ["HB", 0],
+
+	level : 5,
+
+	school : "Evoc",
+
+	time : "1 a",
+
+	range : "self (60 ft)",
+
+	components : "V,S,M",
+
+	compMaterial : "Small Crystals",
+
+	duration : "Instantaneous",
+
+	save : "Dex",
+
+	description : "4d8 slashing or bludgeoning damage. 4d8+1d8/SL lightning damage on failed save.",
+
+	descriptionFull : "Holding crystals in your hand, your hand starts to channel electricity. The crystals start statically forming into shapes guided by your electricity that are either sharp or blunt in nature. With a deep breath and exhalation you cause statically charged crystals to erupt from your hands in a 60-foot cone. Each creature in this 60-foot cone must make a Dexterity saving throw. Any creature hit by this spell takes 4d8 slashing or bludgeoning damage (spellcaster chooses based on crystal shape). Any creature failing the Dexterity saving throw additionally takes 4d8 lightning damage." + "\n   " +"At Higher Levels: When you cast this spell using a spell slot of 6th level or higher, the lightning damage increases by 1d8 for each slot level about 5th.",
 
 	ritual : false,
 
