4e7df059b59611e525fe44b3842313db1a4f19a3
Clayton Turner <clayton.turner@sproutsocial.com>
2022-08-30 13:23:00 -0500

    Fix Sourcelist info for EI Group Maneuvers

--- EI/group-maneuvers.js
+++ EI/group-maneuvers.js
@@ -1,8 +1,8 @@
 var iFileName = "group-maneuvers.js";
 RequiredSheetVersion(12.999);
 
-SourceList["CT:EI:M"] = {
-    name : "ClaytonTurner: Ethereal Incursion Maneuevers",
+SourceList["CT:EI"] = {
+    name : "ClaytonTurner: Ethereal Incursion",
     abbreviation : "CT:EI",
     date : "2022/08/30"
 }
@@ -11,9 +11,9 @@ SourceList["CT:EI:M"] = {
 // Known issue: Does not automatically appear in Limited Features
 ClassList.barbarian.features["group maneuvers"] = {
     name : "Group Maneuver",
-    source : ["CT:EI:M", 0],
+    source : ["CT:EI", 3],
     minlevel : 1,
-    usages: levels.map(function (n) {
+    usages : levels.map(function (n) {
                 return n < 5 ? 2 : n < 9 ? 3 : n < 13 ? 4 : n < 17 ? 5 : 6;
             }),
     recovery : "long rest",
@@ -23,13 +23,13 @@ ClassList.barbarian.features["group maneuvers"] = {
     extraname : "Group Maneuever",
     extrachoices : ["Reflect Magic", "Whirling Magic"],
     "reflect magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Reflect Magic",
         description : "\n    " + "If an ally makes a ranged spell attack against a target within 10 feet of you and misses, you can use your reaction to redirect the magic back toward the target. Make a new attack roll using your proficiency bonus and your Dexterity or spellcasting ability modifier (your choice).",
         action : ["reaction", ""]
     },
     "whirling magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Whirling Magic",
         description : "\n    " + "When an ally you can see or hear within 30 feet of you makes a melee weapon attack against a creature, you can use your reaction to enchant your ally's weapon, allowing them to create a damaging burst of force with their weapon instead. In lieu of the attack, your ally can force each creature within 5 feet of themself to succeed on a Dexterity saving throw or take force damage equal to a damage roll made with the weapon, plus additional force damage equal to your spellcasting modifier.",
         action : ["reaction", ""]
@@ -38,7 +38,7 @@ ClassList.barbarian.features["group maneuvers"] = {
 
 ClassList.druid.features["group maneuvers"] = {
     name : "Group Maneuver",
-    source : ["CT:EI:M", 0],
+    source : ["CT:EI", 3],
     minlevel : 1,
     usages: 2,
     recovery : "long rest",
@@ -48,13 +48,13 @@ ClassList.druid.features["group maneuvers"] = {
     extraname : "Group Maneuever",
     extrachoices : ["Reflect Magic", "Whirling Magic"],
     "reflect magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Reflect Magic",
         description : "\n    " + "If an ally makes a ranged spell attack against a target within 10 feet of you and misses, you can use your reaction to redirect the magic back toward the target. Make a new attack roll using your proficiency bonus and your Dexterity or spellcasting ability modifier (your choice).",
         action : ["reaction", ""]
     },
     "whirling magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Whirling Magic",
         description : "\n    " + "When an ally you can see or hear within 30 feet of you makes a melee weapon attack against a creature, you can use your reaction to enchant your ally's weapon, allowing them to create a damaging burst of force with their weapon instead. In lieu of the attack, your ally can force each creature within 5 feet of themself to succeed on a Dexterity saving throw or take force damage equal to a damage roll made with the weapon, plus additional force damage equal to your spellcasting modifier.",
         action : ["reaction", ""]
@@ -63,7 +63,7 @@ ClassList.druid.features["group maneuvers"] = {
 
 ClassList.wizard.features["group maneuvers"] = {
     name : "Group Maneuver",
-    source : ["CT:EI:M", 0],
+    source : ["CT:EI", 3],
     minlevel : 1,
     usages: 2,
     recovery : "long rest",
@@ -73,13 +73,13 @@ ClassList.wizard.features["group maneuvers"] = {
     extraname : "Group Maneuever",
     extrachoices : ["Reflect Magic", "Whirling Magic"],
     "reflect magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Reflect Magic",
         description : "\n    " + "If an ally makes a ranged spell attack against a target within 10 feet of you and misses, you can use your reaction to redirect the magic back toward the target. Make a new attack roll using your proficiency bonus and your Dexterity or spellcasting ability modifier (your choice).",
         action : ["reaction", ""]
     },
     "whirling magic": {
-        source : ["CT:EI:M", 0],
+        source : ["CT:EI", 3],
         name: "Whirling Magic",
         description : "\n    " + "When an ally you can see or hear within 30 feet of you makes a melee weapon attack against a creature, you can use your reaction to enchant your ally's weapon, allowing them to create a damaging burst of force with their weapon instead. In lieu of the attack, your ally can force each creature within 5 feet of themself to succeed on a Dexterity saving throw or take force damage equal to a damage roll made with the weapon, plus additional force damage equal to your spellcasting modifier.",
         action : ["reaction", ""]
