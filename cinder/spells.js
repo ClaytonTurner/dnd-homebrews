593a1db2b3770fc30a9ff63b6af0f5cba17e0f60
Clayton <clayton.turner@sproutsocial.com>
2024-10-15 19:40:01 -0500

    Create spells.js for Cinder

---
+++ cinder/spells.js
@@ -0,0 +1,31 @@
+SpellsList["elementalism"] = {
+	name : "Elementalism",
+	classes : ["sorcerer"],
+	source : ["PHB", 1],
+	level : 0,
+	school : "Trans",
+	time : "1 a",
+	range : "30 ft",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "Create a minor elemental effect",
+	descriptionFull : "You exert control over the elements, creating one of the following effects within range: \n *Beckon Air.* You create a breeze strong enough to ripple cloth, stir dust, rustle leaves, and close open doors and shutters, all in a 5-foot cube. Doors and shutters being held open by someone or something aren’t affected. \n *Beckon Earth.* You create a thin shroud of dust or sand that covers surfaces in a 5-foot-square area, or you cause a single word to appear in your handwriting in a patch of dirt or sand. \n *Beckon Fire.* You create a thin cloud of harmless embers and colored, scented smoke in a 5-foot cube. You choose the color and scent, and the embers can light candles, torches, or lamps in that area. The smoke’s scent lingers for 1 minute. \n *Beckon Water.* You create a spray of cool mist that lightly dampens creatures and objects in a 5-foot cube. Alternatively, you create 1 cup of clean water either in an open container or on a surface, and the water evaporates in 1 minute. \n *Sculpt Element.* You cause dirt, sand, fire, smoke, mist, or water that can fit in a 1-foot cube to assume a crude shape (such as that of a creature or an object) for 1 hour.",
+	ritual : false,
+	psionic : false,
+};
+
+SpellsList["sorcerous burst"] = {
+	name : "Sorcerous Burst",
+	classes : ["sorcerer"],
+	source : ["PHB", 1],
+	level : 0,
+	school : "Evoc",
+	time : "1 a",
+	range : "120 ft",
+	components : "V,S",
+	duration : "Instantaneous",
+	description : "1 crea 1d8 <Elem> dmg; on d8, roll another d8",
+	descriptionFull : "You cast sorcerous energy at one creature or object within range. Make a ranged attack roll against the target. On a hit, the target takes 1d8 damage of a type you choose: Acid, Cold, Fire, Lightning, Poison, Psychic, or Thunder. \n If you roll an 8 on a d8 for this spell, you can roll another d8, and add it to the damage. When you cast this spell, the maximum number of these d8s you can add to the spell’s damage equals your spellcasting ability modifier. \n  The damage increases by 1d8 when you reach levels 5 (2d8), 11 (3d8), and 17 (4d8).",
+	ritual : false,
+	psionic : false,
+};
